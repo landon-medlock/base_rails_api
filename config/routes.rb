8d5fe36a218b1a606452a7f92996cf8a1affbf83
Landon Medlock <landon.medlock@gmail.com>
2016-10-27 12:58:55 -0700

    versioning api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
+
   mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :comments
+    end
+  end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
